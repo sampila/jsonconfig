1dba1a31db771360c29964da35990397a0a47fe7
sampila <alipxsulistio@gmail.com>
2019-12-20 17:53:47 +0700

    fixing

--- jsonconfig/jsonconfig.go
+++ jsonconfig/jsonconfig.go
@@ -13,7 +13,54 @@ type Parser interface {
 	ParseJSON([]byte) error
 }
 
+// Parser must implement ParseJSON
+type Server struct {
+	Hostname     string        `json:"hostname"`
+	UseHTTP      bool          `json:"usehttp"`
+	UseHTTPS     bool          `json:"usehttps"`
+	HTTPPort     int           `json:"httpport"`
+	HTTPSPort    int           `json:"httpsport"`
+	CertFile     string        `json:"certfile"`
+	KeyFile      string        `json:"keyfile"`
+	ReadTimeout  time.Duration `json:"readtimeout"`
+	WriteTimeout time.Duration `json:"writetimeout"`
+}
+
+type Database struct{
+	Driver string `json:"driver"`
+	Host 	 string `json:"host"`
+	Port	 string	`json:"port"`
+	DB		 string `json:"db"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Client struct {
+	Port   int    `json:"port"`
+	Prefix string `json:"prefix"`
+}
+
+type Backend struct {
+	Port   int    `json:"port"`
+	Prefix string `json:"prefix"`
+}
+
+// configuration contains the application settings
+type Configuration struct {
+	Session  interface{} 		 `json:"Session"`
+	Database Database  	 		 `json:"Database"`
+	Server   Server          `json:"Server"`
+	Client   Client          `json:"Client"`
+	Backend  Backend         `json:"Backend"`
+	Env      string          `json:"Env"`
+	BaseUrl  string          `json:"BaseUrl"`
+}
+// ParseJSON unmarshals bytes to structs
+func (c *Configuration) ParseJSON(b []byte) error {
+	return json.Unmarshal(b, &c)
+}
 // Load the JSON config file
+
 func Load(configFile string, p Parser) error {
 	var err error
 	var input = io.ReadCloser(os.Stdin)
@@ -35,42 +82,4 @@ func Load(configFile string, p Parser) error {
     return err
 	}
   return nil
-}
-
-// Parser must implement ParseJSON
-type Server struct {
-	Hostname     string        `json:"hostname"`
-	UseHTTP      bool          `json:"usehttp"`
-	UseHTTPS     bool          `json:"usehttps"`
-	HTTPPort     int           `json:"httpport"`
-	HTTPSPort    int           `json:"httpsport"`
-	CertFile     string        `json:"certfile"`
-	KeyFile      string        `json:"keyfile"`
-	ReadTimeout  time.Duration `json:"readtimeout"`
-	WriteTimeout time.Duration `json:"writetimeout"`
-}
-
-type Client struct {
-	Port   int    `json:"port"`
-	Prefix string `json:"prefix"`
-}
-
-type Backend struct {
-	Port   int    `json:"port"`
-	Prefix string `json:"prefix"`
-}
-
-// configuration contains the application settings
-type Configuration struct {
-	Session  interface{} 		 `json:"Session"`
-	Database interface{}  	 `json:"Database"`
-	Server   Server          `json:"Server"`
-	Client   Client          `json:"Client"`
-	Backend  Backend         `json:"Backend"`
-	Env      string          `json:"Env"`
-	BaseUrl  string          `json:"BaseUrl"`
-}
-// ParseJSON unmarshals bytes to structs
-func (c *Configuration) ParseJSON(b []byte) error {
-	return json.Unmarshal(b, &c)
 }
